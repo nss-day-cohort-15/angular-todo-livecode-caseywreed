64dfc0d2e89ea58f24bb577154d057c2bbbebfb2
Casey Reed <casey.w.reed@gmail.com>
2016-09-01 11:11:16 -0500

    Search bar moved

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -2,11 +2,14 @@
 
 //Make a new controller on the App module
 //Two args: Name, Function. Inject $scope into the function
-app.controller("NavCtrl", function($scope, SearchTermData){
+app.controller("NavCtrl", function($scope, SearchTermData, $location){
     $scope.searchText = SearchTermData
     $scope.navItems = [
         {url: "#/logout", name: "Logout"},
         {url: "#/items/list", name: "All Items"},
         {url: "#/items/new", name: "New Items"}
     ]
+
+    $scope.isActive = (viewLocation) => viewLocation === $location.path()
+
 })
